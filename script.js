f90a8c6aaecb54c90a7c8c58a63c1a611cbb891c
Kellen Hunter-La Voy <hunterlavoy4@gmail.com>
2018-04-25 18:30:51 -0500

    ..

---
+++ script.js
@@ -0,0 +1,13 @@
+// This is the recommendation on how to create DIVs with a .js file
+
+// var div = document.createElement("div");
+// div.style.width = "100px";
+// div.style.height = "100px";
+// div.style.background = "red";
+// div.style.color = "white";
+// div.innerHTML = "Hello";
+
+// document.getElementById("main").appendChild(div);
+// OR
+// document.body.appendChild(div);
+// Use parent reference instead of document.body.
