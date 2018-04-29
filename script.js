316573cedf2ed94d12cbbbdb79e60d4c31a19cfe
Kellen Hunter-La Voy <hunterlavoy4@gmail.com>
2018-04-29 15:54:44 -0500

    Create createGrid() function to create a new grid of divs within a container div of a given size

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-
-let container = document.getElementById("container");
-
 function createGrid(x) {
-	for (var rows = 0; rows < x; rows++) {
-		for (var columns = 0; columns < x; columns++) {
-			container.append("div class='grid'></div>");
+	let container = document.getElementById("container");
+	for (let i = 0; i < x; i++) {
+		for (let j = 0; j < x; j++) {
+			let divs = document.createElement("div").innerHTML = "<div class='grid'></div>";
+			divs.className = "grid";
+			container.append("div");
 		};
 	};
-	document.getElementsByClassName("grid").width(960/x);
-	document.getElementsByClassName("grid").width(960/x);
+	document.getElementsByClassName("grid").width = 960 / x;
+	document.getElementsByClassName("grid").height = 960 / x;
 }
 
 createGrid(16);
-
