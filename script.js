a75ff5454d7157b21cc0f0470a0d2faca7a10041
Kellen Hunter-La Voy <hunterlavoy4@gmail.com>
2018-05-01 11:47:13 -0500

    Add descriptive notes

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// This function creates a grid using a given input and 
+// changes the background color of the divs that make 
+// up the grid when they are moused over.
 function createGrid(x) {
 	let container = document.getElementById("container");
 	for (let i = 0; i < x; i++) {
@@ -21,18 +24,26 @@ function createGrid(x) {
 	});
 }
 
+// This loads a 16x16 grid automatically when the page 
+// is loaded.
 window.onload=createGrid(16);
 
 
-
+// Function to clear the grid, removing the divs inside of
+// the container.
 function clearGrid() {
 	document.getElementById("container").innerHTML = "";
 }
 
+// Function to reset the grid, first clearing it, then
+// storing a user-entered number and creating a new grid
+// using the user's seletion.
 function reset() {
 	clearGrid();
 	let z = prompt("Pick a number...");
 	createGrid(z);
 }
 
+// This adds an event listener to the 'reset' button in the 
+// html document, calling the reset() function when it is clicked.
 document.getElementById("reset").addEventListener("click", reset);
